2b149516c667138deb9fe4d419f50febfa106e22
Marcos Caceres <marcosscaceres@gmail.com>
2012-10-27 17:44:57 +0100

    Switching to use hellogeri's google analytics account

--- scripts/ga.js
+++ scripts/ga.js
@@ -1,9 +1,10 @@
 //Dynamically loads google analytics, but only on github. 
 //Google analyitics is not allowed on the W3C because of their privacy policy. 
-if(window.location.host === "responsiveimagescg.github.com"){
+if(window.location.host.search("responsiveimages.org") >= 0){
 	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-35523667-1']);
+	  _gaq.push(['_setAccount', 'UA-35760540-1']);
 	  _gaq.push(['_trackPageview']);
+	  _gaq.push(['_setDomainName', 'responsiveimages.org']);
 	
 	  (function() {
 		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
